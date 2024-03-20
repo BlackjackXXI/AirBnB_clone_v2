55a850fbe33a50537e21a1323eac0b5d062076c6
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 15:50:47 -0700

    user.py updated

--- models/user.py
+++ models/user.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-"""user update"""
+"""This is the user class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
- 
-class User(BaseModel, Base):
-    """ This class represents a user with the following attributes:
-    email, password, name and last name
-    """
 
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+
+    email = Column(String(128),
+                   nullable=False)
+
+    password = Column(String(128),
+                      nullable=False)
+
+    first_name = Column(String(128),
+                        nullable=True)
+
+    last_name = Column(String(128),
+                       nullable=True)
+
+    places = relationship("Place",
+                          backref="user",
+                          cascade="all, delete-orphan")
+
+    reviews = relationship("Review",
+                           backref="user",
+                           cascade="all, delete-orphan")
