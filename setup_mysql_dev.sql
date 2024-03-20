4dc98d09d89aa5504f996fbf2ef917ef77240ca9
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 14:45:10 -0700

    sql updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
--- Create the database if not already existing
+-- prepares a MySQL server for this project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user hbnb_dev
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
--- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+       -- Note: password of >>hbnb_dev_pwd<< specified by project reqs
+GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
-FLUSH PRIVILEGES;
