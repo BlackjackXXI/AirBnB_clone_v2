688d5b81392d2a275c6bda3bc080e419f9d76ecd
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 15:52:41 -0700

    user.py updated

--- models/user.py
+++ models/user.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
+"""user update"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-
+from models.base_model import BaseModel, Base
+ 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+
+    """ This class represents a user with the following attributes:
+    email, password, name and last name
     """
+
     __tablename__ = "users"
-
-    email = Column(String(128),
-                   nullable=False)
-
-    password = Column(String(128),
-                      nullable=False)
-
-    first_name = Column(String(128),
-                        nullable=True)
-
-    last_name = Column(String(128),
-                       nullable=True)
-
-    places = relationship("Place",
-                          backref="user",
-                          cascade="all, delete-orphan")
-
-    reviews = relationship("Review",
-                           backref="user",
-                           cascade="all, delete-orphan")
+    
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
