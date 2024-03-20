800e512d165affa6277ab7fd239ec187cd2e1b9c
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 14:57:56 -0700

    sql updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
---a script that prepares a MySQL server for the project
+--prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 --The password of hbnb_test should be set to hbnb_test_pwd
+USE hbnb_dev_db;
 GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+FLUSH PRIVILEGES;
