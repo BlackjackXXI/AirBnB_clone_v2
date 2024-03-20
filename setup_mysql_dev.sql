6cf3e75b78fb663cc29ce2b500647133b85326d9
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 14:51:05 -0700

    sql updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,4 @@
--- prepares DB if not there
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create User if not already there
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
