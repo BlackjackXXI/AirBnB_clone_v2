1615a17f60a64f0c1518974f3e448ee4277db1b6
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-20 15:46:51 -0700

    user.py updated

--- models/user.py
+++ models/user.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel, Base
+"""user update"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
  
-""" This class represents a user with the following attributes:
-email, password, name and last name
-"""
 class User(BaseModel, Base):
+    """ This class represents a user with the following attributes:
+    email, password, name and last name
+    """
+
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
